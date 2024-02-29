431eff4c5bb2d15544b6a3ffc1c824b55c412a9b
premsingh8171 <premsingh8171@gmail.com>
2024-02-29 23:10:51 +0530

    Add .NET MAUI Project

---
+++ NikeZebraRFIDMauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NikeZebraRFIDMauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
